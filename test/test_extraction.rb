a1b66301e960c1970b65d6e8f3a37f39e64a195e
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-03-01 09:54:10 +0000

    added test for support for non File based IO objects

--- test/test_extraction.rb
+++ test/test_extraction.rb
@@ -8,34 +8,42 @@ class TestExtraction < Test::Unit::TestCase
   
   include SysMODB::SpreadsheetExtractor
   
-  def test_from_file_object    
+  def test_from_file_object
     test_sheet = File.dirname(__FILE__) + "/files/test-spreadsheet.xls"
-    f=open(test_sheet,"rb")    
-    xml = spreadsheet_to_xml(f)     
-    assert_not_nil xml      
+    f=open(test_sheet,"rb")
+    xml = spreadsheet_to_xml(f)
+    assert_not_nil xml
+  end
+
+  def test_from_non_file_io_object
+    test_sheet = File.dirname(__FILE__) + "/files/test-spreadsheet.xls"
+    contents = open(test_sheet, "rb") {|io| io.read }
+    io=StringIO.new contents
+    assert_nil io.path
+    xml = spreadsheet_to_xml(io)
   end
   
   def test_validate_xml
     test_sheet = File.dirname(__FILE__) + "/files/test-spreadsheet.xls"
-    f=open(test_sheet,"rb")    
+    f=open(test_sheet,"rb")
     xml = spreadsheet_to_xml(f)
     validate_against_schema(xml)
   end
-  
+
   def test_failure
     test_sheet = File.dirname(__FILE__) + "/files/not-a-spreadsheet.xls"
     f=open(test_sheet,"rb")
-    assert_raise SysMODB::SpreadsheetExtractionException do 
-      spreadsheet_to_xml(f)      
+    assert_raise SysMODB::SpreadsheetExtractionException do
+      spreadsheet_to_xml(f)
     end
   end
-  
+
   def validate_against_schema xml
-    document = LibXML::XML::Document.string(xml)    
-    schema = LibXML::XML::Schema.new(SCHEMA_FILE_PATH)    
+    document = LibXML::XML::Document.string(xml)
+    schema = LibXML::XML::Schema.new(SCHEMA_FILE_PATH)
     begin
       document.validate_schema(schema)
-    rescue LibXML::XML::Error => e          
+    rescue LibXML::XML::Error => e
       puts xml
       assert false,"Error validating against schema: #{e.message}"
     end
@@ -45,7 +53,7 @@ class TestExtraction < Test::Unit::TestCase
     test_sheet = File.dirname(__FILE__) + "/files/test-spreadsheet.xls"
     expected_file = File.dirname(__FILE__) + "/files/test-csv-output1.csv"
     expected = open(expected_file,"rb").read
-    
+
     f=open(test_sheet,"rb")
     csv = spreadsheet_to_csv(f,2)
     assert_equal expected,csv
@@ -65,14 +73,14 @@ class TestExtraction < Test::Unit::TestCase
 #    csv = spreadsheet_to_csv(f,2,true)
 #    assert_equal expected,csv
 #  end
-  
-  def test_for_segfault  
+
+  def test_for_segfault
     test_sheet = File.dirname(__FILE__) + "/files/test-spreadsheet.xls"
     10.times do |x|
-      f=open(test_sheet,"rb")    
-      xml = spreadsheet_to_xml(f)     
-    end    
-    true 
+      f=open(test_sheet,"rb")
+      xml = spreadsheet_to_xml(f)
+    end
+    true
   end
   
 end
