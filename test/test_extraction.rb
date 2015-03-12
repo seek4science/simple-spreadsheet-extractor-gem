e0d68e3fb386882f5288b809e123819143ddf698
Stuart Owen <stuart.owen@manchester.ac.uk>
2015-03-12 17:36:56 +0000

    added test case for xlsx

--- test/test_extraction.rb
+++ test/test_extraction.rb
@@ -7,13 +7,6 @@ class TestExtraction < Test::Unit::TestCase
   SCHEMA_FILE_PATH = File.dirname(__FILE__) + "/../doc/schema-v1.xsd"
   
   include SysMODB::SpreadsheetExtractor
-  
-  def test_from_file_object
-    test_sheet = File.dirname(__FILE__) + "/files/test-spreadsheet.xls"
-    f=open(test_sheet,"rb")
-    xml = spreadsheet_to_xml(f)
-    assert_not_nil xml
-  end
 
   def test_from_non_file_io_object
     test_sheet = File.dirname(__FILE__) + "/files/test-spreadsheet.xls"
@@ -24,6 +17,13 @@ class TestExtraction < Test::Unit::TestCase
   
   def test_validate_xml
     test_sheet = File.dirname(__FILE__) + "/files/test-spreadsheet.xls"
+    f=open(test_sheet,"rb")
+    xml = spreadsheet_to_xml(f)
+    validate_against_schema(xml)
+  end
+
+  def test_validate_xml_xlsx
+    test_sheet = File.dirname(__FILE__) + "/files/test-spreadsheet.xlsx"
     f=open(test_sheet,"rb")
     xml = spreadsheet_to_xml(f)
     validate_against_schema(xml)
